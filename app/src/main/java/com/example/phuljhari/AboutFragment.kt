713c158c5887b3e9171289aadbdee3d39e9d1c54
1719p <1719pankaj@gmail.com>
2022-06-02 13:46:18 +0530

    Fixed theming, fixed night mode, added about

--- app/src/main/java/com/example/phuljhari/AboutFragment.kt
+++ app/src/main/java/com/example/phuljhari/AboutFragment.kt
@@ -1,10 +1,14 @@
 package com.example.phuljhari
 
+import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_about.view.*
 
 
 class AboutFragment : Fragment() {
@@ -13,7 +17,29 @@ class AboutFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_about, container, false)
 
+        val nightModeFlags = requireContext().resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        when (nightModeFlags) {
+            Configuration.UI_MODE_NIGHT_YES -> view.imageView.setImageDrawable(resources.getDrawable(R.drawable.github_white))
+            Configuration.UI_MODE_NIGHT_NO -> view.imageView.setImageDrawable(resources.getDrawable(R.drawable.github_logo))
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> view.imageView.setImageDrawable(resources.getDrawable(R.drawable.github_logo))
+        }
+
+        view.imageView.setOnClickListener{
+            openGithub()
+        }
+
+
+
+
+
         return view
+    }
+    //open a browser to the github page.
+    fun openGithub() {
+        val url = "https://github.com/1719pankaj/Phuljhari"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
     }
 
 }
